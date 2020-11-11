ef8e073d484496fa312af37528cfe21b5a51021a
datthaprasad <datthaprasad29@gmail.com>
2020-11-11 09:50:37 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import {BrowserRouter,Redirect,Switch,Route} from 'react-router-dom'
+import Login from './routes/Login';
+import Signup from './routes/Signup';
 function App() {
+
+  
+  // const [UserId,setUserID]=useState(null);
+  // const [Phonen,setPhone]=useState(null);
+  // const [Playerss,setPlayers]=useState([]);
+  // const [isLogedIn,SetLogedIn]=useState(false);
+   
+
+  // const Login1 = useCallback((id,phone,players)=>{
+  //   console.log(id+" "+phone+" "+players);
+  //   setUserID(id);
+  //   SetLogedIn(true);
+  //   setPhone(phone);
+  //   setPlayers(players)
+  // },[]);
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div>   
+      <React.Fragment>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login' exact>
+            <Login/>
+          </Route>
+          <Route path='/signup' exact>
+            <Signup/>
+          </Route>
+          <Route path='/companies' exact>
+            {/* <Comapny /> */}
+          </Route>
+          <Route path='/courses' exact>
+            {/* <Course/> */}
+          </Route>
+          <Route path='/logout' >
+  
+          </Route>
+          <Redirect to='/'/>
+        </Switch>
+      </BrowserRouter>
+      </React.Fragment>
+      </div> 
+    
   );
-}
+  }
 
 export default App;
---
+++ src/routes/Signup.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const Signup=()=>{
+    return(
+        
+            <h1>this is Signup page</h1>
+        
+    )
+}
+export default Signup;
