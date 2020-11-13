9c37aab8aea962667001666372c5057be6ee12df
datthaprasad <datthaprasad29@gmail.com>
2020-11-13 13:06:59 +0530

    login and signup is created

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './Login.css';
+
 const Login=()=>{
+
     return(
-        
-            <h1>this is login page</h1>
-        
+
+        <div className="login-box">
+            <div className="textbox">
+                <i className="fas fa-user"/>
+                <input type="text" placeholder="Emial"/>
+            </div>
+
+            <div className="textbox">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Password"/>
+            </div>
+
+        <input type="button" class="btn" value="Sign in"/>
+        </div>
     )
 }
+
 export default Login;
--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -2,7 +2,25 @@ import React from 'react'
 const Signup=()=>{
     return(
         
-            <h1>this is Signup page</h1>
+        <div className="login-box">
+            {/* <h1>Sign-up</h1> */}
+            <div className="textbox">
+                <i className="fas fa-user"/>
+                <input type="text" placeholder="Email"/>
+            </div>
+
+            <div className="textbox">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Password"/>
+            </div>
+
+            <div className="textbox">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Confirm Password"/>
+            </div>
+
+        <input type="button" class="btn" value="Sign up"/>
+        </div>
         
     )
 }
